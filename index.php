f3307b24c51225ffd0410f2e65fedfb6fb997a9d
Gabriel da Luz <gabrielbarbosaweb7@gmail.com>
2018-08-07 05:57:14 -0400

    Init 1.0

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+	
+	/* Created by Gabriel A. Barbosa */
+	date_default_timezone_set('America/Campo_Grande');
+	
+	/* Incluindo o autoload do Composer para carregar a biblioteca */
+	require_once 'class/Mysqldump.php';
+	 
+	/* Incluindo a classe que criamos para controlar o backup */
+	require_once 'class/controlBackup.class.php';
+	 
+	/* Como a geração do backup pode ser demorada, retiramos */
+	/* o limite de execução do script */
+	set_time_limit(0);
+	
+	/* Utilizando a classe para gerar um backup na pasta 'backups' */
+	/* e manter os últimos dez arquivos */
+	$backup = new BackupDatabase('/home/namecpanel/public_html/AutoBackupMysql/backups', 20);
+	$backup->setDatabase('localhost', 'user', 'databasename', 'senha');
+	$backup->generate();
+	
+	/* Setar a pasta no ftp com permissao 770 para todos os arquivos e pastas - para não ficar public */
+
+?>
